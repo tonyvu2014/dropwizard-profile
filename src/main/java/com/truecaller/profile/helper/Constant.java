25f4c0b984dab1f0cb15e3921a571d82c4b8b1c7
Tony Vu <tonyvu2008@gmail.com>
2016-08-15 21:55:56 +0800

    Remove redundant file

--- src/main/java/com/truecaller/profile/helper/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.truecaller.profile.helper;
-
-public final class Constant {
-	public static final int MAX_RECENT_VIEW_RECORD = 10;
-	public static final int MAX_RECENT_VIEW_DAY = 10;
-	public static final String DISPLAY_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
-}
